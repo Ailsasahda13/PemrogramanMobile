c4d613191d3f58725a0ef17c6f55928d6ae5729c
Ailsasahda13 <ailsashda13@gmail.com>
2026-02-11 14:36:51 +0700

    first commit

---
+++ bin/dart_basic1.dart
@@ -0,0 +1,7 @@
+//commant untuk mengimport(include)
+import 'package:dart_basic1/dart_basic1.dart' as dart_basic1;
+
+void main(List<String> arguments) { 
+  print('Hello world: ${dart_basic1.calculate()}!');
+  print('Luas Segitiga: ${dart_basic1.calculateLuasSegitiga()}!');
+}
---
+++ lib/dart_basic1.dart
@@ -0,0 +1,11 @@
+int calculate() {
+  return 6 * 7;
+}
+
+double calculateLuasSegitiga() {
+  return 0.5 * 6 * 7;
+}
+
+
+
+
