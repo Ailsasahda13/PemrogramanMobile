5170b099d888fa9ee6300aa7a34ac5f4aef0945b
Ailsasahda13 <ailsashda13@gmail.com>
2026-02-11 21:31:20 +0700

    minggu 1

---
+++ dart_basic1/bin/dart_basic1.dart
@@ -0,0 +1,9 @@
+//commant untuk mengimport(include)
+import 'package:dart_basic1/dart_basic1.dart' as dart_basic1;
+
+void main(List<String> arguments) { 
+  print('Hello world: ${dart_basic1.calculate()}!');
+
+  //tugas
+  print('Triangle Area: ${dart_basic1.TriangleArea()}!');
+}
---
+++ dart_basic1/lib/dart_basic1.dart
@@ -0,0 +1,16 @@
+int calculate() {
+  return 6 * 7;
+}
+
+//tugas menghitung luas segitiga
+int TriangleArea() {
+  int base = 13;
+  int height = 20;
+
+  return (base * height) ~/ 2;
+}
+
+
+
+
+
