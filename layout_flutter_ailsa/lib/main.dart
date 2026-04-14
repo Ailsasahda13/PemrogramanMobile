b896cd3fad84adacd1ddc09409d5a3bdb26fe29b
Ailsasahda13 <ailsashda13@gmail.com>
2026-04-14 09:35:32 +0700

    langkah 1: build method column

--- layout_flutter_ailsa/lib/main.dart
+++ layout_flutter_ailsa/lib/main.dart
@@ -54,9 +54,30 @@ class MyApp extends StatelessWidget {
           title: const Text('Flutter layout demo'),
         ),
 
-        // HANYA bagian ini yang diganti
         body: titleSection,
       ),
+    );
+  }
+
+  // Method build Button Column
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
